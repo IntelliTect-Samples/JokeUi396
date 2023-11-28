7e3a2f76b21f9f30e8a27fd662ec2efe767a9568
philspokas <phil@intellitect.com>
2023-11-27 22:09:47 -0800

    set env

--- build/tools/scratch.ps1
+++ build/tools/scratch.ps1
@@ -1,3 +1,6 @@
+# https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Credentials/appId/875a99e3-f3f2-4db4-8c51-f6ff26ee5b32/isMSAApp~/false
+
+
 az deployment group create --resource-group Joke-dev --template-file .\build\env\main.bicep
 
 $key = az staticwebapp secrets list --name JokeUi396-dev --query "properties.apiKey" -o tsv
